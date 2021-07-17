9a0a808cd5080a5a4c8f1823f01a7fa4c6308a0b
cublllll <87566560+cublllll@users.noreply.github.com>
2021-07-17 12:39:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 nums = [16, 8, 26, 4, 1, 3]
-nums.sort()
-print('List in Ascending Order: ', nums)
-
-nums.sort(reverse=True)
-print('List in Descending Order: ', nums)
+print(nums.sort(),nums)
+print(nums.sort(reverse=True), nums)
